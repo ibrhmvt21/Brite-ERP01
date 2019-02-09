cd2b2fa5aa14adeea58a1618cafc8a1361aa8b01
Ibrahim <barankrsln13@gmail.com>
2019-02-09 17:45:37 -0500

    k

--- src/test/java/com/events/utilities/BrowserUtils.java
+++ src/test/java/com/events/utilities/BrowserUtils.java
@@ -68,6 +68,7 @@ public class BrowserUtils {
      * returns to original window if windows with given title not found
      */
     public static void switchToWindow(String targetTitle) {
+       // String origin = Driver.getDriver().getWindowHandle();
         String origin = Driver.getDriver().getWindowHandle();
         for (String handle : Driver.getDriver().getWindowHandles()) {
             Driver.getDriver().switchTo().window(handle);
